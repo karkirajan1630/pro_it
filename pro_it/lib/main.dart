30d986bcf8f7d55ca625f9446236b81a4290926e
aistha11 <bijayshrestha0908@gmail.com>
2022-02-24 21:16:19 +0545

    Updated

--- pro_it/lib/main.dart
+++ pro_it/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Marketing proo",
+      title: "Pro IT",
       theme: ThemeData(
         primaryColor: Pallete.primaryCol,
         backgroundColor: Pallete.backgroundColor,
